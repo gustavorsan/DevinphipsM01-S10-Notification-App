fe79c53aa1a04f0da554752089c35ef01b65bfb8
gustavorsan <gustavorsantos42@gmail.com>
2022-12-11 20:04:45 -0300

    Configuração inicial do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
