d53561af196b439b9f2831e8365cee15e5912700
gustavorsan <gustavorsantos42@gmail.com>
2022-12-12 07:46:11 -0300

    resolução do ex06 ex08 ex09 e correção do ex10

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,18 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit  {
   @Output() filterEvent = new EventEmitter<string>();
+  filterType = ''
 
+  constructor(private router: Router,private route :  ActivatedRoute){}
 
-  filterType = 'todos';
-
-  sendFilterType(type: 'todos'|'naoLidos'|'lidos'){
-    this.filterType = type;
+  ngOnInit(): void {
+    this.sendFilterType(this.route.snapshot.params['type'])
+  }
+  sendFilterType(type: 'todos'|'nao-lidos'|'lidos'){
+     this.filterType = type;
+     this.router.navigate(['/home',type])
      this.filterEvent.emit(type);
   }
 
